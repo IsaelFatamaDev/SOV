ea61f956a8eca91d78d97da7db043d33bfbd6f38
IsaelFatamaDev <isael.fatama@vallegrande.edu.pe>
2023-10-31 16:48:05 -0500

    v1.1
    Server con NodeJs, agregando nueva manera de crear el servidor web

--- Rutas/index.js
+++ Rutas/index.js
@@ -16,4 +16,22 @@ app.listen(port, () => {
       console.log(`Se está ejecutando sobre el puerto http://localhost:${port}`);
 });
 
+/* 
+// TODO: IMPLEMENTACION DE NODE JS - EXPRESS 
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const ip = 'localhost';
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+      console.log(`Se está ejecutando sobre el puerto http://${ip}:${port}`);
+});
+
+app.get('/inicio.html', function (req, res) {
+      var file = path.join(__dirname, '/public/index.html');
+      res.sendFile(file);
+})
+*/
