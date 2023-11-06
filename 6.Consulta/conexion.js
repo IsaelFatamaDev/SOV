d48051d864d5b32163cb8f5cdcd3ea1e8e78d42f
IsaelFatamaDev <isael.fatama@vallegrande.edu.pe>
2023-11-06 16:36:24 -0500

    Consulta con MYSQL, EXPRESS, NODEJS

--- 6.Consulta/conexion.js
+++ 6.Consulta/conexion.js
@@ -7,14 +7,52 @@ var cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-var con = mysql.createConnection({
+var conexion = mysql.createConnection({
       host: "localhost",
       user: "isael",
       password: "admin",
       database: "sakila"
 });
 
-con.connect(function (err) {
+conexion.connect(function (err) {
       if (err) throw err;
       console.log("Conexion exitosa");
+});
+
+conexion.query('SELECT * FROM rental', function (err, result, field) {
+      if (err) throw err;
+      result.forEach(element => {
+            console.log(element)
+      });
+})
+
+conexion.query('SELECT * FROM staff', function (err, result, field) {
+      if (err) throw err;
+      result.forEach(element => {
+            console.log(element)
+      });
+})
+
+
+const query = "SHOW TABLES";
+
+conexion.query(query, (err, results) => {
+      if (err) {
+            console.error("Error al ejecutar la consulta:", err);
+            return;
+      }
+
+      console.log("Tablas en la base de datos:");
+      results.forEach((row) => {
+            console.log(row[`Tables_in_${conexion.config.database}`]);
+      });
+
+});
+
+conexion.end((err) => {
+      if (err) {
+            console.error("Error al cerrar la conexión:", err);
+      } else {
+            console.log("Conexión cerrada.");
+      }
 });
