89a55ec9a05034150174acca5b02e2559f6d23a8
IsaelFatamaDev <isael.fatama@vallegrande.edu.pe>
2023-11-09 16:01:27 -0500

    1.3

--- Login/login.js
+++ Login/login.js
@@ -20,30 +20,24 @@ app.use(session({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'style.css')));
 
-// http://localhost:3000/
 app.get('/', function(request, response) {
-	// Render login template
+
 	response.sendFile(path.join(__dirname + '/login.html'));
 });
 
-// http://localhost:3000/auth
 app.post('/auth', function(request, response) {
-	// Capture the input fields
+
 	let username = request.body.username;
 	let password = request.body.password;
-	// Ensure the input fields exists and are not empty
+
 	if (username && password) {
-		// Execute SQL query that'll select the account from the database based on the specified username and password
 		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			// If there is an issue with the query, output the error
 			if (error) throw error;
-			// If the account exists
 			if (results.length > 0) {
-				// Authenticate the user
 				request.session.loggedin = true;
 				request.session.username = username;
-				// Redirect to home page
 				response.redirect('/home');
 			} else {
 				response.send('Usuario y/o Contraseña Incorrecta');
@@ -56,14 +50,10 @@ app.post('/auth', function(request, response) {
 	}
 });
 
-// http://localhost:3000/home
 app.get('/home', function(request, response) {
-	// If the user is loggedin
 	if (request.session.loggedin) {
-		// Output username
 		response.send('Te has logueado satisfactoriamente:, ' + request.session.username + '!');
 	} else {
-		// Not logged in
 		response.send('¡Inicia sesión para ver esta página!');
 	}
 	response.end();
